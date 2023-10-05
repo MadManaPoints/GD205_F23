241e2422147459cf09999936027d1ca58a615b64
MadManaPoints <mannyvivas21@gmail.com>
2023-10-05 02:40:12 -0400

    terrain and obstacles added to UAP scene

--- Assets/Script/PhysicsMovement.cs
+++ Assets/Script/PhysicsMovement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PhysicsMovement : MonoBehaviour
 {
     Rigidbody rb;
-    public float force_mult = 5f;
+    public float force_mult = 20f;
+    public float maxSpeed = 100f; 
+
+    bool moveForward;
+    bool moveBackward;
+    bool moveLeft;
+    bool moveRight;
+    bool moveUp;
+    bool moveDown; 
 
     //HW IS TO CREATE AN ALIEN OBSTACLE COURSE
-
 
     // Start is called before the first frame update
     void Start()
@@ -20,20 +27,64 @@ public class PhysicsMovement : MonoBehaviour
     void Update()
     {
         controls();
+        movePlayer();
+        //limits speed
+        if (rb.velocity.magnitude > maxSpeed){
+			rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+		}
+    }
+
+    void movePlayer(){
+        if (moveForward){
+            rb.AddForce(transform.forward * force_mult);
+        }
+        if (moveLeft){
+            rb.AddForce(-transform.right * force_mult);
+        }
+        if (moveBackward){
+            rb.AddForce(-transform.forward * force_mult);
+        }
+        if (moveRight){
+            rb.AddForce(transform.right * force_mult);
+        }
+        if (moveUp){
+            rb.AddForce(transform.up * force_mult);
+        }
+        if (moveDown){
+            rb.AddForce(-transform.up * force_mult);
+        }
     }
 
     void controls(){
         if (Input.GetKey(KeyCode.W)){
-            rb.AddForce(transform.forward * force_mult);
+            moveForward = true;
+        } else {
+            moveForward = false;
         }
         if (Input.GetKey(KeyCode.A)){
-            rb.AddForce(-transform.right * force_mult);
+            moveLeft = true;
+        } else {
+            moveLeft = false;
         }
         if (Input.GetKey(KeyCode.S)){
-            rb.AddForce(-transform.forward * force_mult);
+            moveBackward = true;
+        } else {
+            moveBackward = false;
         }
         if (Input.GetKey(KeyCode.D)){
-            rb.AddForce(transform.right * force_mult);
+            moveRight = true;
+        } else {
+            moveRight = false;
+        }
+        if (Input.GetKey(KeyCode.E)){
+            moveUp = true;
+        } else {
+            moveUp = false;
+        }
+        if (Input.GetKey(KeyCode.Q)){
+            moveDown = true;
+        } else {
+            moveDown = false;
         }
         if (Input.GetKey(KeyCode.J)){
             rb.velocity *= 0.95f; 
